d34f9615e7405219508224cb1fd893cf8bc7a155
Mateus Oliveira Milane <mateus.milane555@gmail.com>
2023-03-04 09:51:17 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+  /*if(html.classList.contains('light')) {
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }*/
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  //substituir imagem
+  if(html.classList.contains('light')) {
+    // se estiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/avatar-light.png')
+  } else {
+    // se estiver dark mode, manter a imagem padrão
+    img.setAttribute('src', './assets/avatar.png')
+  }
+
+  
+}
